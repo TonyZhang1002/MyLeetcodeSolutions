7fc3ed73f27c47635375827fa008624893e32d62
TonyZhang1002 <z496722204a@hotmail.com>
2019-11-14 11:03:38 +0000

    Problem 2

---
+++ src/Problems/Problem2.java
@@ -0,0 +1,32 @@
+package Problems;
+
+public class Problem2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode pre = dummyHead;
+        boolean preAdd = false;
+        while (l1 != null || l2 != null) {
+            int thisNumber = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + (preAdd ? 1 : 0);
+            if (thisNumber >= 10) {
+                preAdd = true;
+                thisNumber -= 10;
+            } else {
+                preAdd = false;
+            }
+            pre.next = new ListNode(thisNumber);
+            pre = pre.next;
+            if (l1!=null)   l1 = l1.next;
+            if (l2!=null)   l2 = l2.next;
+        }
+        if (preAdd) {
+            pre.next = new ListNode(1);
+        }
+        return dummyHead.next;
+    }
+}
+
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+}
