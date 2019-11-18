db3056bc7c925462daf7142a8a061ec2a86b0f17
TonyZhang1002 <z496722204a@hotmail.com>
2019-11-18 10:22:12 +0000

    Problem 350, optimized 100% faster

--- src/Problems/Problem350.java
+++ src/Problems/Problem350.java
@@ -8,21 +8,23 @@ public class Problem350 {
     public int[] intersect(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        List<Integer> answer = new LinkedList<>();
+        int[] answerArray = new int[Math.min(nums1.length, nums2.length)];
 
         int index1 = 0;
         int index2 = 0;
+        int indexAnswer = 0;
 
         while (index1 < nums1.length && index2 < nums2.length) {
             if (nums1[index1] > nums2[index2]) index2++;
             else if (nums1[index1] < nums2[index2]) index1++;
             else {
-                answer.add(nums1[index1]);
+                answerArray[indexAnswer] = nums1[index1];
                 index1++;
                 index2++;
+                indexAnswer++;
             }
         }
 
-        return answer.stream().mapToInt(i -> i).toArray();
+        return Arrays.copyOfRange(answerArray, 0, indexAnswer);
     }
 }
