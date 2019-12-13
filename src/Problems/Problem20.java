c2918833366172ce5e7dfb05ae3c0c56cccdaf62
TonyZhang1002 <z496722204a@hotmail.com>
2019-12-13 14:45:16 +0000

    Problem 20, dual 98%+

--- src/Problems/Problem20.java
+++ src/Problems/Problem20.java
@@ -1,48 +1,40 @@
 package Problems;
 
-import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Problem20 {
     public static boolean isValid(String s) {
-        int[] flags = new int[s.length()];
-        Arrays.fill(flags, -1);
+        LinkedList<Integer> flags = new LinkedList<>();
         char[] sChars = s.toCharArray();
 
         for(int i = 0; i < sChars.length; i++) {
             if (sChars[i] == 40) {
-                flags[i] = 0;
+                flags.add(0);
             } else if (sChars[i] == 91) {
-                flags[i] = 1;
+                flags.add(1);
             } else if (sChars[i] == 123) {
-                flags[i] = 2;
+                flags.add(2);
             } else if (sChars[i] == 41 || sChars[i] == 93 || sChars[i] == 125) {
                 if (!modifyPreFlag(flags, i, sChars[i]))    return false;
             }
         }
 
-        for (int i : flags) {
-            if(i != -1) return false;
-        }
-        return true;
+        return flags.isEmpty() ? true : false;
     }
 
-    private static boolean modifyPreFlag (int[] flags, int index, char c) {
+    private static boolean modifyPreFlag (LinkedList<Integer> flags, int index, char c) {
         int target = 0;
         if (c == 93)    target = 1;
         else if (c == 125)  target = 2;
-        if(index == 0)  return false;
-        for (int i = index - 1; i > -1; i--) {
-            if (flags[i] != -1 && flags[i] != target) {
-                return false;
-            } else if (flags[i] == target) {
-                flags[i] = -1;
-                break;
-            } else if (i == 0)  return false;
+        if (index == 0 || flags.isEmpty())  return false;
+        if (flags.getLast() != target)  return false;
+        else {
+            flags.removeLast();
+            return true;
         }
-        return true;
     }
 
     public static void main(String[] args) {
-        System.out.println(isValid("]"));
+        System.out.println(isValid(""));
     }
 }
