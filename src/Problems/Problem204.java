c54eaab11fad229c11bdecb7da6c08dc66711a90
TonyZhang1002 <z496722204a@hotmail.com>
2020-01-21 13:37:26 +0000

    Problem 204, not optimized 2

--- src/Problems/Problem204.java
+++ src/Problems/Problem204.java
@@ -4,21 +4,23 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Problem204 {
-    public int countPrimes(int n) {
+    public static int countPrimes(int n) {
+        boolean[] primes = new boolean[n];
         int count = 0;
-        for(int i = 2; i < n; i++){
-            if(isPrime(i)){
-                count++;
-            }
+        for (int i = 0; i< n; i++)
+            primes[i] = true;
+        for(int i = 2; i * i < n; i++)
+        {
+            for(int j = i; j * i < n; j++)
+                primes[j * i]= false;
         }
+        for(int i= 2; i< n; i++)
+            if(primes[i])
+                count++;
         return count;
     }
-    public boolean isPrime(int num){
-        for(int i = 2; i <= (int) Math.sqrt(num); i++){
-            if(num % i == 0){
-                return false;
-            }
-        }
-        return true;
+
+    public static void main(String[] args) {
+        System.out.println(countPrimes(10));
     }
 }
