8b1d1b8c157b90e2674b8c406e6b6c2524b77588
TonyZhang1002 <z496722204a@hotmail.com>
2020-01-23 11:40:23 +0000

    Problem 322

--- src/Problems/Problem204.java
+++ src/Problems/Problem204.java
@@ -1,26 +1,27 @@
 package Problems;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class Problem204 {
     public static int countPrimes(int n) {
-        boolean[] primes = new boolean[n];
-        int count = 0;
-        for (int i = 0; i< n; i++)
-            primes[i] = true;
-        for(int i = 2; i * i < n; i++)
-        {
-            for(int j = i; j * i < n; j++)
-                primes[j * i]= false;
+
+        if (n == 0 || n == 1)   return 0;
+
+        Set<Integer> notPrimeSet = new HashSet<>();
+        for(int i = 2; i * i < n; i++) {
+            for(int j = i; j * i < n; j++) {
+                if (!notPrimeSet.contains(i * j)) {
+                    notPrimeSet.add(i *j);
+                }
+            }
         }
-        for(int i= 2; i< n; i++)
-            if(primes[i])
-                count++;
-        return count;
+        return n - 2 - notPrimeSet.size();
     }
 
     public static void main(String[] args) {
-        System.out.println(countPrimes(10));
+        System.out.println(countPrimes(1500200));
     }
 }
